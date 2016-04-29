dfc0414b244304044dee91e88315be05d8137400
zgl-tech <zhaoguangluan@gmail.com>
2016-04-29 11:05:57 +0800

    Refactor cookies and request.

--- spiders/clan_spider.py
+++ spiders/clan_spider.py
@@ -18,17 +18,34 @@ def _parse_json(text):
         raise RuntimeError(u'body error: %s' % text)
 
 
+def _parse_cookie(cookie_str):
+    cookies = {}
+    for item in cookie_str.split('; '):
+        key, value = item.split('=', 1)
+        cookies[key] = value
+    return cookies
+
+
 class ClanSpider(Spider):
     name = 'clan'
     start_urls = []
 
+    def __init__(self, *largs, **kwargs):
+        self._cookies = _parse_cookie(config.cookie_str)
+        self._headers = config.headers
+        super(ClanSpider, self).__init__(*largs, **kwargs)
+
+    def _make_request(self, url, callback, meta=None):
+        req = Request(url, cookies=self._cookies, headers=self._headers,
+                      callback=callback)
+        if meta:
+            req.meta.update(meta)
+        return req
+
     def start_requests(self):
         for uin in config.start_uins:
             url = _make_url(config.merge_top_url, targetuin=uin)
-            req = Request(url, cookies=config.cookies, headers=config.headers,
-                          callback=self.parse_user)
-            req.meta['uin'] = uin
-            yield req
+            yield self._make_request(url, self.parse_user, {'uin': uin})
 
     def parse_user(self, response):
         data = _parse_json(response.body_as_unicode())
@@ -37,28 +54,20 @@ class ClanSpider(Spider):
             yield followbar
         url = _make_url(config.fans_url, targetuin=response.meta['uin'],
                         start=0, num=config.fans_page_num)
-        req = Request(url, cookies=config.cookies, headers=config.headers,
-                      callback=self.parse_fans)
-        req.meta['uin'] = response.meta['uin']
-        req.meta['start'] = 0
-        yield req
+        yield self._make_request(url, self.parse_fans,
+                                 {'uin': response.meta['uin'], 'start': 0})
 
     def parse_fans(self, response):
         data = _parse_json(response.body_as_unicode())
         for follower in data['result']['follow_list']:
             url = _make_url(config.merge_top_url, targetuin=follower['uin'])
-            req = Request(url, cookies=config.cookies, headers=config.headers,
-                          callback=self.parse_user)
-            req.meta['uin'] = follower['uin']
-            yield req
+            yield self._make_request(url, self.parse_user,
+                                     {'uin': follower['uin']})
 
         if len(data['result']['follow_list']) >= config.fans_page_num:
             return
         start = response.meta['start'] + config.fans_page_num
         url = _make_url(config.fans_url, targetuin=response.meta['uin'],
                         start=start, num=config.fans_page_num)
-        req = Request(url, cookies=config.cookies, headers=config.headers,
-                      callback=self.parse_fans)
-        req.meta['uin'] = response.meta['uin']
-        req.meta['start'] = start
-        yield req
+        yield self._make_request(url, self.parse_fans,
+                                 {'uin': response.meta['uin'], 'start': start})
