39bde2a8a28f8b907f70dd56b322fb7d5515c5f7
RiverJiangtao <jtguo1985@gmail.com>
2016-05-07 22:11:24 +0800

    change name for debug

--- spiders/__init__.py
+++
@@ -1,8 +0,0 @@
-# This package will contain the spiders of your Scrapy project
-#
-# Please refer to the documentation for information on how to create and manage
-# your spiders.
-
-from clan_spider import ClanSpider
-
-__all__ = ['ClanSpider']
