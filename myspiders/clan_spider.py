bd14e6d75e81881fa2ea6d8806e83925c4899506
RiverJiangtao <jtguo1985@gmail.com>
2016-05-07 22:11:24 +0800

    change .gitignore and spider-pipeline directory name

--- myspiders/clan_spider.py
+++ myspiders/clan_spider.py
@@ -48,22 +48,30 @@ class ClanSpider(Spider):
             yield self._make_request(url, self.parse_user, {'uin': uin})
 
     def parse_user(self, response):
+        # getting user information
         uin = str(response.meta['uin'])
         data = _parse_json(response.body_as_unicode())
         user = data['result']['userinfo']
         user['__type'] = 'user'
         user['uin'] = uin
+        # print json.dumps(user,indent=4,ensure_ascii=False)
         yield user
+
+        # getting user-clan following information
         for followbar in data['result']['followbars']:
             followbar['bid'] = str(followbar['bid'])
             followbar['__type'] = 'clan'
             yield followbar
             follow = {'__type': 'follow', 'uin': uin, 'bid': followbar['bid']}
             yield follow
+
+        # getting fans page to parse
         url = _make_url(config.fans_url, targetuin=response.meta['uin'],
                         start=0, num=config.fans_page_num)
         yield self._make_request(url, self.parse_fans,
                                  {'uin': uin, 'start': 0})
+
+        # getting clan following page to parse
 
     def parse_fans(self, response):
         data = _parse_json(response.body_as_unicode())
